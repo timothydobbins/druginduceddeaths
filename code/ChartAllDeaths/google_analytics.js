c2d6734021102e490126859d73cd2f4a08507380
Timothy Dobbins <t.dobbins@unsw.edu.au>
2018-08-09 11:56:31 +1000

    Edit Google Analytics script

--- code/ChartAllDeaths/google_analytics.js
+++ code/ChartAllDeaths/google_analytics.js
@@ -4,17 +4,3 @@
   gtag('js', new Date());
 
   gtag('config', 'UA-123602558-1');
-
-
-// Event Tracking Code
-$(document).on('shiny:inputchanged', function(event) {
-  if(event.name == 'bins' || event.name == 'col'){
-    ga('send', 'event', 'input',
-      'updates', event.name, event.value);
-  }
-});
-
-// User Tracking Code
-$(document).one('shiny:idle', function() {
-  ga('set','userId', Shiny.user);
-});
